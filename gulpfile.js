31719255d156825045192612fa71c6cedc665747
Nate Waddington <nate.waddington@akqa.com>
2018-01-29 11:15:53 -0800

    initial test RGB_LED working 'locally'

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
-var gulp = require('gulp');
-var browser_sync = require('browser-sync').create();
+const gulp = require('gulp');
+const browser_sync = require('browser-sync').create();
 
 // serve
-gulp.task('browser_serve', function() {
-	browser_sync.init({
-		server: {
-			baseDir: 'frontend'
-		},
-	})
+gulp.task('browser_serve', () => {
+  browser_sync.init({
+    server: {
+      baseDir: 'frontend',
+    },
+  });
 });
 
 // auto refresh browser on change
-gulp.task('browser_sync', function(){
+gulp.task('browser_sync', () => {
   browser_sync.reload();
 });
 
 // watcher for the source files
-gulp.task('watch', ['browser_serve','browser_sync'], function () {
-	gulp.watch('frontend/**/*.html', ['browser_sync']); 
-	gulp.watch('frontend/**/*.htm', ['browser_sync']); 
-	gulp.watch('frontend/**/*.css', ['browser_sync']); 
-	gulp.watch('frontend/**/*.js', ['browser_sync']); 
+gulp.task('watch', ['browser_serve', 'browser_sync'], () => {
+  gulp.watch('frontend/**/*.html', ['browser_sync']);
+  gulp.watch('frontend/**/*.htm', ['browser_sync']);
+  gulp.watch('frontend/**/*.css', ['browser_sync']);
+  gulp.watch('frontend/**/*.js', ['browser_sync']);
 });
